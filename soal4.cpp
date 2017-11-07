8078974656c5f72599d76de0ab1aaaeebe639a7d
RikyIrawan <32484719+RikyIrawan@users.noreply.github.com>
2017-11-07 23:21:43 +0700

    membuat percabangan nya untuk mengetahui bilangan yg telah di inputkan oleh user

--- soal4.cpp
+++ soal4.cpp
@@ -5,5 +5,16 @@ int main(){
 	int bil;
 	
 	cout<<"Input Bilangan : ";cin>>bil;
+	if (bil < 0)
+	{
+		cout<<"bilangan yang anda input negatif"<<endl;
+	}
+    else if (bil>0)
+	{
+		cout<<"bilangan yang anda input positif"<<endl;
+	}
+    else
+	
+		cout<<"bilangan yang anda input nol "<<endl;
 
 }
