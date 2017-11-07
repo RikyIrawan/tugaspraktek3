fd0a3fb8c95d1cf55ad66f8c7ef2eeea84d2dc30
RikyIrawan <32484719+RikyIrawan@users.noreply.github.com>
2017-11-07 21:52:33 +0700

    menanpilkan hitungan dari inputan user

--- soal3.cpp
+++ soal3.cpp
@@ -17,6 +17,10 @@ int main(){
 	jkt=40+jl;
 	gt=jkt*gpj;
 	gb=gt-(0.15*gt);
-	
+	cout <<"jam lembur anda:"<<l;
+	cout <<"bonus dari lembur anda:"<<jl;
+	cout <<"jam kerja total anda:"<<jkt;
+	cout <<"gaji total anda:"<<gt;
+	cout <<"gaji bersih anda di potong pajak:"<<gb;
 	
 }
