a23d03273b97ac0cbb60ca4022086802927dbc4f
RikyIrawan <32484719+RikyIrawan@users.noreply.github.com>
2017-11-07 21:26:44 +0700

    membuat percabangandengan ketentuan yg telah ditentukan

--- soal2.cpp
+++ soal2.cpp
@@ -9,6 +9,25 @@ int main(){
 	
 	cout << "luas apa yang ingin anda hitung inputkan 'p' untuk persegi dan 's' untuk segitiga"; cin >> huruf;
 	
+	if (huruf=='p' || huruf=='P' )
+	{
+		cout <<"Anda akan menghitung luas persegi \n"
+		 <<"masukan tinggi:"; cin >> tinggi;
+		cout <<"masukan lebar:"; cin >> lebar;
+		cout <<"luas persegi anda:";
+		cout <<tinggi*lebar;
+		}
+		else	if (huruf=='s' || huruf=='S' )
+	{
+		cout <<"Anda akan menghitung luas segitiga \n"
+		 <<"masukan tinggi:"; cin >> tinggi;
+		cout <<"masukan alas:"; cin >> alas;
+		cout <<"luas segitiga anda:";
+		cout <<0.5*alas*tinggi;
+		}
+		else
+		cout <<"huruf yang anda input tidak tersedia";
+		
 	
 	
 }
